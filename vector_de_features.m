8b2f72349399d69e4e660a9f9177d638903e4cca
Carolina Migliorelli <cmigliorelli@gmail.com>
2018-07-18 13:58:18 +0200

    pequeños cambios

--- vector_de_features.m
+++ vector_de_features.m
@@ -2,15 +2,16 @@
 
 subject='PAT_7-EEG_1050';
 
-results_path='C:\Users\Caro\Documents\MATLAB\TFGs\TFG-SEIZURE\results';
+results_path='/Users/carolinamigliorelli/Documents/MATLAB/TFG-SEIZURE/results';
 
 feature_names_f={'Power','delta','theta','alpha','beta','gammalow','gammahigh','ripple1','ripple2','entropy','rms','mfrec'};
 
 
-load([results_path filesep subject])
+load([results_path subject filesep 'features.mat'])
 
 features_promedio=permute(mean(features,2),[3 1 2]);
 
 
 features_all=[features_promedio; es_crisis_win];feature_names_f={'Power','delta','theta','alpha','beta','gammalow','gammahigh','ripple1','ripple2','entropy','rms','mfrec'};
 
+features_all=features_all';
